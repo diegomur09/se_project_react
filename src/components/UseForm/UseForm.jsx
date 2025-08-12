b124bdd3ccc1a9694ed233d24c5e91afe6b51a2e
diego_mur <diegomurcia2@gmail.com>
2025-08-11 21:58:33 -0600

    Final 2

--- src/components/UseForm/UseForm.jsx
+++ src/components/UseForm/UseForm.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-export function useForm(initialValues) {
+export function UseForm(initialValues) {
   const [values, setValues] = useState(initialValues);
 
   const handleChange = (event) => {
